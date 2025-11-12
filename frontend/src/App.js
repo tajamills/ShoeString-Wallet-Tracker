024851772895d3a7caeafa7af6020ce41234a4c2
ShoeString User <you@example.com>
2025-11-12 03:20:01 +0000

    auto-commit for 5b3f3e5a-7cf5-4046-8930-e2bd7dfc0ce9

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -696,7 +696,7 @@ function App() {
                           <th className="text-left py-3 px-4 text-gray-400 font-medium">Hash</th>
                           <th className="text-left py-3 px-4 text-gray-400 font-medium">Asset</th>
                           <th className="text-right py-3 px-4 text-gray-400 font-medium">Amount</th>
-                          <th className="text-left py-3 px-4 text-gray-400 font-medium">Address</th>
+                          <th className="text-left py-3 px-4 text-gray-400 font-medium">Address/Label</th>
                         </tr>
                       </thead>
                       <tbody>
