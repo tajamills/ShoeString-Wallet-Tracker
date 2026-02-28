c8523a6c812b5acc1af794a86a51c82f9d027d2f
ShoeString User <you@example.com>
2026-02-28 17:03:10 +0000

    auto-commit for 273a304c-8d1f-4b25-83e6-7535f708cf7b

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,6 +16,7 @@ import { ChainRequestModal } from '@/components/ChainRequestModal';
 import { ExportModal } from '@/components/ExportModal';
 import { TaxDashboard } from '@/components/TaxDashboard';
 import { TransactionCategorizer } from '@/components/TransactionCategorizer';
+import { ScheduleDExport, BatchCategorizationModal } from '@/components/TaxEnhancements';
 
 const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
 const API = `${BACKEND_URL}/api`;
